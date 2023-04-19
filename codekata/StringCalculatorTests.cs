b168265161fb80e0d0b105593d07d72525d76463
Your Name <you@example.com>
2023-04-19 11:10:28 +0200

    Initial commit

---
+++ codekata/StringCalculatorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core.Internal;
+using FluentAssertions;
+using Xunit;
+
+namespace CodeKata
+{
+    public class StringCalculatorTests
+    {
+
+    }
+}
